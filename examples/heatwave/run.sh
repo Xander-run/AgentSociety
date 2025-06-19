de9bc8eca47ddb03df2f2d282b552edd1a680bcb
TianLu <tian.lu@enterprisedb.com>
2025-06-19 16:00:18 +0800

    chore fix run.sh

--- examples/heatwave/run.sh
+++ examples/heatwave/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+
+eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
