ab89904f3207ba46277c8430eabb388d301fd336
TianLu <tian.lu@enterprisedb.com>
2025-06-19 15:59:05 +0800

    chore fix run.sh

--- examples/heatwave/run.sh
+++ examples/heatwave/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
 pyenv activate as-137
